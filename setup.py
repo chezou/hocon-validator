0a805664f67b8d56081a486c2c793d929d7f189e
Aki Ariga <chezou+github@gmail.com>
2017-08-14 15:00:59 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,20 @@ import setuptools
 
 setuptools.setup(
     name='hocon-validator',
+    description='A HOCON validator',
+    author='Aki Ariga chezou',
+    author_email='chezou@gmail.com',
+    license='Apache License 2.0',
+    keywords='HOCON validator',
     version='0.0.1',
-    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=[
         'pyyaml',
         'pyhocon',
